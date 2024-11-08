cfe7c23ce3ee8761484dcc0ce64ae1e49cb83f36
Your Name <you@example.com>
2024-11-08 13:29:07 +0300

    Тестовый

--- main-service/src/main/java/ru/practicum/central/exception/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.central.exception;
-
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
