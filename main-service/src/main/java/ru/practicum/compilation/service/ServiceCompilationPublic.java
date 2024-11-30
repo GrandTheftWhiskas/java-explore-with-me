3ede853d0155e1d589500e4aacd2d084ff146b46
Your Name <you@example.com>
2024-11-30 11:58:58 +0300

    Основной сервис

--- main-service/src/main/java/ru/practicum/compilation/service/ServiceCompilationPublic.java
+++ main-service/src/main/java/ru/practicum/compilation/service/ServiceCompilationPublic.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.compilation.dto.CompilationResponse;
 import ru.practicum.compilation.dto.EventByCompId;
 import ru.practicum.compilation.mapper.CompilationMapper;
