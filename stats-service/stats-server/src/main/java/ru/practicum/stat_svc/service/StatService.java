a28d4711a447cfbd907c903820cd8805b4e5b117
Your Name <you@example.com>
2024-11-30 11:09:08 +0300

    Основной сервис

--- stats-service/stats-server/src/main/java/ru/practicum/stat_svc/service/StatService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stat_svc/service/StatService.java
@@ -2,10 +2,7 @@ package ru.practicum.stat_svc.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.practicum.EventDto;
 import ru.practicum.StatsDto;
 import ru.practicum.exception.*;
